f7bf517eabd7e4691cb85038f318009c7ab7d659
Josh Febbie <joshdfebbie@gmail.com>
2022-11-02 20:00:43 -0700

    second-attempt

--- server/controllers/pageCtrl.js
+++ server/controllers/pageCtrl.js
@@ -2,6 +2,6 @@ const path = require("path")
 
 module.exports = {
     home: (req, res) => {
-        res.sendFile(path.join(__dirname, "../..index.html" ))
+        res.sendFile(path.join(__dirname, "../../index.html"))
     }
 }
